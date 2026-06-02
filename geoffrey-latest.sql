aa64f17fe78dba2bbf6577a6097775b67b1dd11c
Geoffrey Backup <geoffrey@inosfnbg3p>
2026-06-02 03:17:01 +0200

    geoffrey backup 20260602-011701

--- geoffrey-latest.sql
+++ geoffrey-latest.sql
@@ -15,6 +15,11 @@ INSERT INTO "audit_log" VALUES(4,'2026-05-31 23:50:48','ingestion','account_crea
 INSERT INTO "audit_log" VALUES(5,'2026-05-31 23:50:49','ingestion','account_created','6a1cc959e73224705',NULL,NULL);
 INSERT INTO "audit_log" VALUES(6,'2026-05-31 23:51:30','ingestion','account_created','6a1cc982b4c3c2ab1',NULL,NULL);
 INSERT INTO "audit_log" VALUES(7,'2026-05-31 23:51:32','ingestion','account_created','6a1cc9843a57d9736',NULL,NULL);
+CREATE TABLE backfill_ledger (
+    message_id   TEXT PRIMARY KEY,
+    action       TEXT NOT NULL,
+    processed_at TEXT NOT NULL
+);
 CREATE TABLE drafts (
             id               INTEGER PRIMARY KEY,
             agent_name       TEXT NOT NULL,
@@ -99,7 +104,7 @@ CREATE TABLE mailbox_cursor (
             last_message_id  TEXT,
             updated_at       TEXT NOT NULL DEFAULT (datetime('now'))
         );
-INSERT INTO "mailbox_cursor" VALUES('inbox','2026-05-31T22:53:07Z','AAMkADA1MWU5NDk2LTY1YjktNGMwMy05ZmFiLWFlOTZlMTk4MTU1YQBGAAAAAAATbMc9knsQRIAnAvTKh8PnBwBjh9rTbUXdTKA8-fOASe6cAAAAAAEMAABjh9rTbUXdTKA8-fOASe6cAAia4Lh7AAA=','2026-05-31 23:51:22');
+INSERT INTO "mailbox_cursor" VALUES('inbox','2026-06-01T21:31:33Z','AAMkADA1MWU5NDk2LTY1YjktNGMwMy05ZmFiLWFlOTZlMTk4MTU1YQBGAAAAAAATbMc9knsQRIAnAvTKh8PnBwBjh9rTbUXdTKA8-fOASe6cAAAAAAEMAABjh9rTbUXdTKA8-fOASe6cAAia4LiOAAA=','2026-06-01 21:47:23');
 INSERT INTO "mailbox_cursor" VALUES('sentitems','2026-05-30T05:01:48Z','AAMkADA1MWU5NDk2LTY1YjktNGMwMy05ZmFiLWFlOTZlMTk4MTU1YQBGAAAAAAATbMc9knsQRIAnAvTKh8PnBwBjh9rTbUXdTKA8-fOASe6cAAAAAAEJAABjh9rTbUXdTKA8-fOASe6cAAiZ9kR0AAA=','2026-05-31 23:52:09');
 CREATE TABLE pending_mail_drafts (
             id          TEXT PRIMARY KEY,
