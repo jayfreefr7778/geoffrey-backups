f5db8919f828644507cc067e144db1dc3c49100c
Geoffrey Backup <geoffrey@inosfnbg3p>
2026-06-11 03:17:02 +0200

    geoffrey backup 20260611-011701

--- geoffrey-latest.sql
+++ geoffrey-latest.sql
@@ -118,8 +118,8 @@ CREATE TABLE mailbox_cursor (
             last_message_id  TEXT,
             updated_at       TEXT NOT NULL DEFAULT (datetime('now'))
         );
-INSERT INTO "mailbox_cursor" VALUES('inbox','2026-06-09T19:09:36Z','AAMkADA1MWU5NDk2LTY1YjktNGMwMy05ZmFiLWFlOTZlMTk4MTU1YQBGAAAAAAATbMc9knsQRIAnAvTKh8PnBwBjh9rTbUXdTKA8-fOASe6cAAAAAAEMAABjh9rTbUXdTKA8-fOASe6cAAihKlm7AAA=','2026-06-09 19:48:11');
-INSERT INTO "mailbox_cursor" VALUES('sentitems','2026-06-09T14:01:49Z','AAMkADA1MWU5NDk2LTY1YjktNGMwMy05ZmFiLWFlOTZlMTk4MTU1YQBGAAAAAAATbMc9knsQRIAnAvTKh8PnBwBjh9rTbUXdTKA8-fOASe6cAAAAAAEJAABjh9rTbUXdTKA8-fOASe6cAAihKvjjAAA=','2026-06-09 15:48:11');
+INSERT INTO "mailbox_cursor" VALUES('inbox','2026-06-10T17:01:53Z','AAMkADA1MWU5NDk2LTY1YjktNGMwMy05ZmFiLWFlOTZlMTk4MTU1YQBGAAAAAAATbMc9knsQRIAnAvTKh8PnBwBjh9rTbUXdTKA8-fOASe6cAAAAAAEMAABjh9rTbUXdTKA8-fOASe6cAAih14EYAAA=','2026-06-10 17:48:16');
+INSERT INTO "mailbox_cursor" VALUES('sentitems','2026-06-10T14:01:50Z','AAMkADA1MWU5NDk2LTY1YjktNGMwMy05ZmFiLWFlOTZlMTk4MTU1YQBGAAAAAAATbMc9knsQRIAnAvTKh8PnBwBjh9rTbUXdTKA8-fOASe6cAAAAAAEJAABjh9rTbUXdTKA8-fOASe6cAAih18-OAAA=','2026-06-10 15:48:17');
 CREATE TABLE pending_mail_drafts (
             id          TEXT PRIMARY KEY,
             to_addr     TEXT NOT NULL,
@@ -597,6 +597,7 @@ INSERT INTO "xray_seen" VALUES('https://ceinterim.com/fr/1358-expert-en-finance-
 INSERT INTO "xray_seen" VALUES('https://nordicinterim.com/articles/dont-be-afraid-to-try','https://nordicinterim.com/articles/dont-be-afraid-to-try/','“Don’t be afraid to try”','brave','2026-06-09 14:01:49');
 INSERT INTO "xray_seen" VALUES('https://borsaitaliana.it/borsa/notizie/radiocor/finanza/dettaglio/economia-e-finanza-gli-avvenimenti-di-giovedi--11-giugno-nRC_08062026_1931_669140076.html','https://www.borsaitaliana.it/borsa/notizie/radiocor/finanza/dettaglio/economia-e-finanza-gli-avvenimenti-di-giovedi--11-giugno-nRC_08062026_1931_669140076.html','Economia e finanza: gli avvenimenti di GIOVEDI'' 11 giugno - Borsa Italiana','brave','2026-06-09 14:01:49');
 INSERT INTO "xray_seen" VALUES('https://lequipe.fr/Football/Article/L-annonce-d-un-mysterieux-accord-avec-lassana-diarra-ne-met-pas-fin-a-l-action-collective-engagee-contre-la-fifa/1683118','https://www.lequipe.fr/Football/Article/L-annonce-d-un-mysterieux-accord-avec-lassana-diarra-ne-met-pas-fin-a-l-action-collective-engagee-contre-la-fifa/1683118','Malgré l''annonce d''un mystérieux accord avec Lassana Diarra, la FIFA n''en a pas terminé avec cette histoire et reste sous la menace d''une action collective','brave','2026-06-09 14:01:49');
+INSERT INTO "xray_seen" VALUES('https://nordicinterim.se/artiklar/var-inte-radd-for-att-prova','https://nordicinterim.se/artiklar/var-inte-radd-for-att-prova/','”Var inte rädd för att prova”','brave','2026-06-10 14:01:50');
 CREATE INDEX idx_drafts_status        ON drafts(status);
 CREATE INDEX idx_drafts_published_at  ON drafts(published_at);
 CREATE INDEX idx_audit_timestamp      ON audit_log(timestamp);
